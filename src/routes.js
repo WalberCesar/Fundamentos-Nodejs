b4f8a7a52498f5e2a21fc23b06522dda98a9e1be
Walber Cesar de sousa santos <walber.santos01@etec.sp.gov.br>
2023-03-25 13:38:35 -0300

    query

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ export const routes = [
     path: buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users')
+      console.log(req.query)
 
       return res.end(JSON.stringify(users))
     },
@@ -37,6 +38,21 @@ export const routes = [
       const { id } = req.params
 
       database.delete('users', id)
+
+      return res.writeHead(204).end()
+    },
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+
+      database.update('users', id, {
+        name,
+        email,
+      })
 
       return res.writeHead(204).end()
     },
