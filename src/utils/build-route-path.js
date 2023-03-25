dac1080e0bac5fb75d260ed6013f7be8046c7281
Walber Cesar de sousa santos <walber.santos01@etec.sp.gov.br>
2023-03-25 13:04:34 -0300

    create method Update

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,7 +1,13 @@
-export function buildRoutePath(path) {
-  const routeParametersRegex = /:([a-zA-Z]+)/g;
-  const pathWithParams = path.replaceAll(routeParametersRegex, "([a-z0-9-_]+)");
+// /users/:id
 
-  const pathRegex = new RegExp(`^${pathWithParams}`);
-  return pathRegex;
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-Z]+)/g
+  const pathWithParams = path.replaceAll(
+    routeParametersRegex,
+
+    '(?<$1>[a-z0-9\\-_]+)',
+  )
+
+  const pathRegex = new RegExp(`^${pathWithParams}`)
+  return pathRegex
 }
