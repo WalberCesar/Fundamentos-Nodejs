5bafdbaf87f462d6b6709a9a291875de64a49538
Walber Cesar de sousa santos <walber.santos01@etec.sp.gov.br>
2023-03-25 12:53:41 -0300

    create regex, route delete and route params

--- src/middlewares/database.js
+++ src/middlewares/database.js
@@ -1,39 +1,48 @@
-import fs from "node:fs/promises";
+import fs from 'node:fs/promises'
 
-const databasePath = new URL("../db.json", import.meta.url);
+const databasePath = new URL('../db.json', import.meta.url)
 
 export class Database {
-  #database = {};
+  #database = {}
 
   constructor() {
-    fs.readFile(databasePath, "utf-8")
+    fs.readFile(databasePath, 'utf-8')
       .then((data) => {
-        this.#database = JSON.parse(data);
+        this.#database = JSON.parse(data)
       })
       .catch(() => {
-        this.#persist();
-      });
+        this.#persist()
+      })
   }
 
   #persist() {
-    fs.writeFile(databasePath, JSON.stringify(this.#database));
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
 
   select(table) {
-    const data = this.#database[table] ?? [];
+    const data = this.#database[table] ?? []
 
-    return data;
+    return data
   }
 
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
-      this.#database[table].push(data);
+      this.#database[table].push(data)
     } else {
-      this.#database[table] = [data];
+      this.#database[table] = [data]
     }
 
-    this.#persist();
+    this.#persist()
 
-    return data;
+    return data
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
   }
 }
